02ef3d452102613fbfc489b82f41ebd03e823564
Matheus da Cruz <matheuswachcruz@gmail.com>
2021-07-12 11:04:16 -0300

    feat: initial config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,24 @@
 import { AppProps } from 'next/app';
 import Router from 'next/router';
 import Nprogress from 'nprogress';
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyle } from 'styles/global';
+import { theme } from 'styles/theme';
 
 Router.events.on('routeChangeStart', () => Nprogress.start());
 Router.events.on('routeChangeComplete', () => Nprogress.done());
 Router.events.on('routeChangeError', () => Nprogress.done());
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,16 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap'
+            rel='stylesheet'
+          />
           <link rel='stylesheet' type='text/css' href='/nprogress.css' />
           <link
             rel='shortcut icon'
@@ -18,5 +24,30 @@ export default class MyDocument extends Document {
         </body>
       </Html>
     );
+  }
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
