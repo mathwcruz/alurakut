e3492dba0169b74d792f7da77c8bf11db2c02629
Matheus da Cruz <matheuswachcruz@gmail.com>
2021-07-13 09:22:42 -0300

    feat: crate comunity form and box

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react';
+import { FormEvent, useMemo, useState } from 'react';
 import { AlurakutMenu, OrkutNostalgicIconSet } from 'lib/AlurakutCommons';
 import { ProfileSidebar } from 'components/ProfileSidebar';
 
@@ -11,15 +11,16 @@ import { ProfileRelationsBoxWrapper } from 'styles/components/ProfileRelations';
 interface favoritesPeopleData {
   id: string;
   userName: string;
-  avatar_url: string;
+  avatarUrl: string;
 }
 
-export default function Home() {
+//TODO: estilizar página 404
+
+export default function Home({ githubUserName = 'mathwcruz' }) {
   const [favoritesPeople, setFavoritesPeople] = useState<favoritesPeopleData[]>(
     []
   );
-
-  const githubUserName = 'mathwcruz';
+  const [comunities, setComunities] = useState(['Alurakut']);
 
   useMemo(async () => {
     const { data } = await api.get(
@@ -30,14 +31,20 @@ export default function Home() {
       return {
         id: people?.id,
         userName: people?.login,
-        avatar_url: people?.avatar_url,
+        avatarUrl: people?.avatar_url,
       };
     });
 
     setFavoritesPeople(favoritesPeople);
   }, []);
 
-  console.log({ favoritesPeople });
+  function handleCreateComunity(e: FormEvent) {
+    e.preventDefault();
+
+    console.log({ e });
+
+    setComunities([...comunities]);
+  }
 
   return (
     <>
@@ -51,7 +58,31 @@ export default function Home() {
             <h1 className='title'>Bem-vindo(a)</h1>
             <OrkutNostalgicIconSet />
           </Box>
+
+          {/* componentizar formulário */}
+          <Box>
+            <h1 className='subTitle'>O que você deseja fazer?</h1>
+            <form onSubmit={handleCreateComunity}>
+              <div>
+                <input
+                  type='text'
+                  name='title'
+                  placeholder='Qual vai ser o nome da sua comunidade?'
+                  aria-label='Qual vai ser o nome da sua comunidade?'
+                />
+              </div>
+              <div>
+                <input
+                  name='image'
+                  placeholder='Coloque uma URL para usarmos de capa'
+                  aria-label='Coloque uma URL para usarmos de capa'
+                />
+              </div>
+              <button type='submit'>Criar comunidade</button>
+            </form>
+          </Box>
         </div>
+        {/* componentizar ProfileRelations */}
         <div
           className='profileRelations'
           style={{ gridArea: 'profileRelations' }}
@@ -64,14 +95,30 @@ export default function Home() {
               {favoritesPeople?.map((person) => (
                 <li key={person?.id}>
                   <a href={`/users/${person?.userName}`}>
-                    <img src={person?.avatar_url} alt={person?.userName} />
+                    <img src={person?.avatarUrl} alt={person?.userName} />
                     <span>{person?.userName}</span>
                   </a>
                 </li>
               ))}
             </ul>
           </ProfileRelationsBoxWrapper>
-          <Box>Comunidade</Box>
+          {/* componentizar ProfileComunities */}
+          <ProfileRelationsBoxWrapper>
+            <h2 className='smallTitle'>Comunidades ({comunities?.length})</h2>
+            <ul>
+              {comunities?.map((comunity) => (
+                <li key={comunity}>
+                  <a href={`https://github.com/mathwcruz`}>
+                    <img
+                      src={`https://github.com/mathwcruz.png`}
+                      alt={`Matheus da Cruz`}
+                    />
+                    <span>{comunity}</span>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </ProfileRelationsBoxWrapper>
         </div>
       </MainGrid>
     </>
